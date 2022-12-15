fae1b0bc9824f3740b3164da76ee3a350532cbd0
mhtjung <themikejung@gmail.com>
2022-12-15 11:00:06 -0800

    Adding files
    Co-Authored-By: Aashish Balaji <87250229+aashish-b@users.noreply.github.com>
    Co-Authored-By: Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
    Co-Authored-By: Jordan Swartz <10202436+jordanLswartz@users.noreply.github.com>

--- README.md
+++
@@ -1,1 +0,0 @@
-# team-triage.github.io
---
+++ assets/scripts/logo-btn.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute("src", "assets/images/logo-color.png");
+}
+
+function unhover(element) {
+  element.setAttribute("src", "assets/images/logo-color.png");
+}
